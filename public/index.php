c88157ec66f2277cd5b289d781022c1329a92a69
brm_perezd <alexander.perez@brm.com.co>
2017-07-04 17:01:50 -0500

    Añadido html a index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
-<?php 
-	
-?>
+<!DOCTYPE HTML>
+    <HEAD>
+        <TITLE>Crear carpeta</TITLE>
+    </HEAD>
+
+    <BODY>
+    	<form action="creacarpeta.php" method="post">
+	    	<input type="text" name="nomca">
+	    	<input type="submit" value="Crear">
+	    </form>	
+    </BODY>
+</HTML>
