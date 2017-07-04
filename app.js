2c6e9867a97020ca0f7b89d04b497ff442e07ce5
brm_perezd <alexander.perez@brm.com.co>
2017-07-04 17:54:32 -0500

    agregada linea a app.js

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
