8cf4b1879770eb1ee05394490adc8e7d97a5d614
brm_perezd <alexander.perez@brm.com.co>
2017-07-04 17:58:38 -0500

    Modificado app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var cfenv = require('cfenv');
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function (req, res) {
