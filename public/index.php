8445d457b7a686f87bc1007dfb02230d55a63d3b
brm_perezd <alexander.perez@brm.com.co>
2017-07-04 17:06:30 -0500

    Prueba de index

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
     	<form action="creacarpeta.php" method="post">
 	    	<input type="text" name="nomca">
 	    	<input type="submit" value="Crear">
+	    	<input type="hidden" name="ffff">
 	    </form>	
     </BODY>
 </HTML>
